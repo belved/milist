0b52eb16de2d3466ec3defb7a67fc048c6a55f90
belved <trackun@hotmail.com>
2023-11-29 23:52:57 +0100

    Convert ButtonListSection into a class

--- src/component/buttonListSection.js
+++ src/component/buttonListSection.js
@@ -1,37 +1,46 @@
-import { useState, useEffect } from 'react'
+import React from 'react'
 
 import ButtonList from './buttonList.js'
 
 import { findAll } from '../services/firestoreHelper.js'
 
-function ButtonListSection() {
-    const [tuning, setTuning] = useState([])
-    const [instrument, setInstrument] = useState([])
-    const [style, setStyle] = useState([])
-
-    const fetchData = async () => {
-        setTuning(true)
-
-        const tuningRes = await findAll("tuning")
-        const instrumentRes = await findAll("instruments")
-        const styleRes = await findAll("style")
-
-        setTuning([...tuningRes])
-        setInstrument([...instrumentRes])
-        setStyle([...styleRes])
+class ButtonListSection extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            tuning: {},
+            instrument: {},
+            style: {}
+        }
     }
 
-    useEffect(() => {
+    componentDidMount() {
+        const fetchData = async () => {
+            const tuningRes = await findAll("tuning")
+            const instrumentRes = await findAll("instruments")
+            const styleRes = await findAll("style")
+
+            this.setState({
+                tuning: tuningRes,
+                instrument: instrumentRes,
+                style: styleRes
+            })
+
+            console.log(tuningRes)
+        }
+
         fetchData()
-    }, [])
+     }
     
-    return (
+    render(){
+        return (
       <div>
-          <ButtonList buttonList={tuning}/>
-          <ButtonList buttonList={instrument}/>
-          <ButtonList buttonList={style}/>
+          <ButtonList buttonList={this.state.tuning}/>
+          <ButtonList buttonList={this.state.instrument}/>
+          <ButtonList buttonList={this.state.style}/>
       </div>
     );
   }
+}
 
   export default ButtonListSection
