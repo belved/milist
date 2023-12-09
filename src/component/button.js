97c0b863d7b9c2cadd78f67cddea98cbfca8f1a6
belved <trackun@hotmail.com>
2023-12-09 19:09:23 +0100

    Adding conditional style

--- src/component/button.js
+++ src/component/button.js
@@ -6,6 +6,12 @@ const divStyle = {
   height: '75px'
 }
 
+const selectedButtonStyle = {
+  width: '100px',
+  height: '45px',
+  backgroundColor: 'green'
+}
+
 const buttonStyle = {
   width: '100px',
   height: '45px'
@@ -16,9 +22,15 @@ class Button extends React.Component {
     render() {
       return (
         <div style={divStyle}>
+          {this.props.state ? 
+          <div style={selectedButtonStyle} onClick={() => this.props.onClick()}>
+            {this.props.name}
+          </div> 
+          : 
           <div style={buttonStyle} onClick={() => this.props.onClick()}>
-            {this.props.name} {this.props.state.toString()}
-          </div>
+            {this.props.name}
+          </div>}
+          
         </div>
       );
     }
